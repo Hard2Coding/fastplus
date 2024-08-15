1e4ddc5b99e35b27fed6f0c66500ff49e8166ee5
Hard2Coding <155141031+Hard2Coding@users.noreply.github.com>
2024-08-15 19:38:17 +0700

    Add files via upload
    Update Path V.1.0.2 Fixed resolution bugs

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log('%c' + 'STOP!', 'font-size:50px; ; border-radius: 5px; padding: 20px')
+console.log('%c' + 'Do not paste any code that someone let you paste here if you do not understand that code! This console is for developer purposes only, some code that you received might be attack code that will let someone attack your account called "Self-XSS" Attack', 'font-size:20px; ; border-radius: 5px; padding: 20px')
+console.log('%c' + 'Please use the internet carefully :) ', 'font-size:20px; ; border-radius: 5px; padding: 20px')
+console.log('%c' + 'With best wishes from "FAST+ Developer team" ', 'font-size:20px; ; border-radius: 5px; padding: 20px')
+console.log("Path V.1.0.2")
+
